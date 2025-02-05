a8f9083e916870544e1f5da60c9ea024683ed389
Diamonds85 <2028edenfieldj@cocke.k12.tn.us>
2025-02-05 17:41:39 +0000

    edited neural net structure

--- reinforcement/neural_net.c
+++ reinforcement/neural_net.c
@@ -5,14 +5,15 @@ const int NEURONS[] = {12, 12};
 
 typedef struct neuralnet
 { 
-    double activations[];
-    double biases[];
-    double weights[][];
+    double *activations;
+    double *biases;
+    double **weights;
 };
 
-neuralnet neuralnet_init(const num_layers = NUM_LAYERS, const neurons[] = NEURONS)
+neuralnet initNeuralNet(const num_layers = NUM_LAYERS, const neurons[] = NEURONS)
 {
-
+    neuralnet p_nn;
+    p_nn
 }
 
 int main()
