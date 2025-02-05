27c0b503a624e54f5b53e3c2bf39b6f30b202aab
Diamonds85 <2028edenfieldj@cocke.k12.tn.us>
2025-02-05 17:39:27 +0000

    created basice neural network structure for reinforcement learning

---
+++ reinforcement/neural_net.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+const int NUM_LAYERS = 2;
+const int NEURONS[] = {12, 12};
+
+typedef struct neuralnet
+{ 
+    double activations[];
+    double biases[];
+    double weights[][];
+};
+
+neuralnet neuralnet_init(const num_layers = NUM_LAYERS, const neurons[] = NEURONS)
+{
+
+}
+
+int main()
+{
+
+}
